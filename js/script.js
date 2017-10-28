2f2b0443312478f46fae3c36058a662434579c19
zjykzk <zjykzk@gmail.com>
2017-10-28 15:03:46 +0800

    rebuilding site Sat Oct 28 15:03:46 CST 2017

--- js/script.js
+++ js/script.js
@@ -8,14 +8,31 @@
   'use strict';
 
   // Add a js class to the html-tag when JavsScript is active.
-  $('html').addClass('js');
+  $('html').removeClass('nojs').addClass('js');
 
-  // Activate the SlickNav menu for small screens.
-  if (window.matchMedia && $.isFunction($.fn.slicknav)) {
+  // Activate the mobil menu for small screens.
+  if (window.matchMedia && $('.mobile-nav-wrapper').length) {
     var mq = window.matchMedia('(max-width: 666px)');
     if (mq.matches) {
-      $('.main-menu').hide().find('.navbar').filter(':first').slicknav();
+      // Show mobile menu.
+      $('.mobile-nav-wrapper').removeClass('hidden');
+
+      // Populate the mobile nav sheet.
+      $('.header__region').filter(':not(:empty)').removeClass('header__region region hidden').addClass('js-region').appendTo('.mobile-nav-sheet');
+      $('.main-menu').addClass('hidden').clone().removeClass('main-menu layout__navigation hidden').addClass('js-main-menu').appendTo('.mobile-nav-sheet').find('.navbar').removeClass('navbar').addClass('js-navbar');
+
+      // Toggle the mobile nav sheet.
+      $('.mobile-nav-cover, .mobile-nav-toggle').on('click toushstart', function (e) {
+        e.preventDefault();
+        $('body').scrollTop(0).toggleClass('js-nav-open');
+      });
+
     }
   }
+
+  // Display CSS grid layout warning to old browsers.
+  // if (!window.CSS || !window.CSS.supports || !window.CSS.supports('display', 'grid')) {
+  //   $('.header').after('<div class="layout__grid-notice" role="complementary">Your browsers does not support modern grid layout so this page will not look or behave as it should. Resent versions of Chrome, Firefox and Safari all works so please update. Support is coming in Microsoft Edge as well, until then use another browser. <a href="https://www.whatbrowser.org/">What Browser?</a></div>');
+  // }
 
 })(jQuery);
